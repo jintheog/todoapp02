faaddfa46f54de4f6e5b0ffeb77ec076c8b8eb92
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-18 16:27:54 +0900

    repository setting

---
+++ src/main/java/com/example/todoapp02/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.example.todoapp02.repository;
+
+import com.example.todoapp02.dto.TodoDTO;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class TodoRepository {
+    private final Map<Long, TodoDTO> storage = new ConcurrentHashMap<>();
+    private Long nextId = 1L;
+}
