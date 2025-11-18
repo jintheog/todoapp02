c9ac5ce0c75d7da1ff8261c3c9afdc12b3fe5491
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-18 17:13:23 +0900

    refactor

--- src/main/java/com/example/todoapp02/Todoapp02Application.java
+++ src/main/java/com/example/todoapp02/Todoapp02Application.java
@@ -1,7 +1,11 @@
 package com.example.todoapp02;
 
+import com.example.todoapp02.dto.TodoDTO;
+import com.example.todoapp02.repository.TodoRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Todoapp02Application {
@@ -10,4 +14,12 @@ public class Todoapp02Application {
         SpringApplication.run(Todoapp02Application.class, args);
     }
 
+    @Bean
+    public CommandLineRunner init(TodoRepository todoRepository) {
+        return args -> {
+            todoRepository.save(new TodoDTO(null, "study", "JAVA", false));
+            todoRepository.save(new TodoDTO(null, "cook", "ramen", false));
+            todoRepository.save(new TodoDTO(null, "workout", "running", false));
+        };
+    }
 }
--- src/main/java/com/example/todoapp02/repository/TodoRepository.java
+++ src/main/java/com/example/todoapp02/repository/TodoRepository.java
@@ -1,12 +1,14 @@
 package com.example.todoapp02.repository;
 
 import com.example.todoapp02.dto.TodoDTO;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Repository
 public class TodoRepository {
     private final Map<Long, TodoDTO> storage = new ConcurrentHashMap<>();
     private Long nextId = 1L;
