47bafe191d1efc6c5348a0a795f2b46853e3a7ff
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-18 16:07:04 +0900

    init

---
+++ src/main/java/com/example/todoapp02/Todoapp02Application.java
@@ -0,0 +1,13 @@
+package com.example.todoapp02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Todoapp02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Todoapp02Application.class, args);
+    }
+
+}
